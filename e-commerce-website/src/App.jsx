1bfe69072670a36576103ac26cb572e1815fc284
Cheng Lim <93038309+therealcheng@users.noreply.github.com>
2023-04-04 18:29:49 +0100

    Installed Tailwind

--- e-commerce-website/src/App.jsx
+++ e-commerce-website/src/App.jsx
@@ -1,11 +1,12 @@
 import NavBar from './components/NavBar';
+import './App.css';
 
 function App() {
   return (
-    <div>
+    <div >
       <NavBar />
-      <h1>Lorem Ipsum</h1>
-      <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with
+      <h1 className='text-3xl font-bold underline'>Lorem Ipsum</h1>
+      <p>Lorem ipsum, or ipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De for use in a type specimen book. It usually begins with
       </p>
     </div>
   );
