db5035f6dcd0ca83ecd4060195a1d13fb4c1e808
Cheng Lim <93038309+therealcheng@users.noreply.github.com>
2023-04-04 20:20:18 +0100

    Removed next boiler plate

--- e-commerce-website/src/components/NavBar.jsx
+++ e-commerce-website/src/components/NavBar.jsx
@@ -30,13 +30,12 @@ const NavBar = () => {
       </div>
       <div className="dropdown dropdown-end">
         <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-          <div className="w-10 rounded-full">
+          <div className="w-10 rounded-full"> 
           <img src="/pest-logo.jpg" alt="Pest logo" />
           </div>
         </label>
         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
           <li>
-            <a className="justify-between">
               Profile
               <span className="badge">New</span>
             </a>
