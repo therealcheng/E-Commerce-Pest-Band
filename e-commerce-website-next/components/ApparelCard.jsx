b45656554645022ad1e16513ff2fe4ed03d9f688
Cheng Lim <93038309+therealcheng@users.noreply.github.com>
2023-04-05 18:21:57 +0100

    Prettier format

--- e-commerce-website-next/components/ApparelCard.jsx
+++ e-commerce-website-next/components/ApparelCard.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const ApparelCard = ({name,description, price, src}) => {
+const ApparelCard = ({ name, description, price, src }) => {
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
-  <figure><img src={src} alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">
-      {name}
-      <div className="badge badge-primary">NEW</div>
-      <div className="badge badge-neutral">£{price}</div>
-    </h2>
-    <p>{description}</p>
-    <div className="card-actions justify-end">
-      <div className="badge badge-outline">Fashion</div>
-      <div className="badge badge-outline">Jumper</div>
+      <figure>
+        <img src={src} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {name}
+          <div className="badge badge-primary">NEW</div>
+          <div className="badge badge-neutral">£{price}</div>
+        </h2>
+        <p>{description}</p>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline">Fashion</div>
+          <div className="badge badge-outline">Jumper</div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-  )
-}
+  );
+};
 
-export default ApparelCard
+export default ApparelCard;
