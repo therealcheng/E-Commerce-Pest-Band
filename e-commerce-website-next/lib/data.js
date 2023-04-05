d216d3bc87508750c78765234ebc63ba3030b57d
Cheng Lim <93038309+therealcheng@users.noreply.github.com>
2023-04-05 19:44:45 +0100

    Created Data for products

---
+++ e-commerce-website-next/lib/data.js
@@ -0,0 +1,31 @@
+const data = {
+    products: [
+        {
+            name: 'Jumper',
+            description: 'Welcome to the rebellion.',
+            price: 40,
+            slug: 'pest-jumper',
+            image: '../public/merch-1.png',
+            stock: 10,
+            category: 'Jumper',
+        },
+        {
+            name: 'T-shirt',
+            description: 'Welcome to the rebellion.',
+            price: 20,
+            slug: 'pest-jumper',
+            image: '../public/merch-2.png',
+            stock: 10,
+            category: 'T-shirt',
+        },
+        {
+            name: 'Jumper',
+            description: 'Welcome to the rebellion.',
+            price: 40,
+            slug: 'pest-jumper',
+            image: '../public/flip-1.png',
+            stock: 10,
+            category: 'Jumper',
+        }
+    ]
+}
