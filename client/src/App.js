9dbdba51327308a36b784ffeb13e97c9933f5e74
Cheng Lim <93038309+therealcheng@users.noreply.github.com>
2023-04-15 20:11:42 +0100

    Adding Scenes placeholder and Nav Bar MUI Boxes Styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./scenes/home/Home";
+import ItemDetails from "./scenes/itemDetails/ItemDetails";
+import Checkout from "./scenes/checkout/Checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -19,6 +22,9 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="checkout/success" element={<Confirmation />} />
         </Routes>
       </BrowserRouter>
     </div>
