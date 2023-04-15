f014a0ec1d725d7e7a13ab3b508ae7f8a56dbb9c
Cheng Lim <93038309+therealcheng@users.noreply.github.com>
2023-04-15 20:45:33 +0100

    Moved Navbar to right place and configured import inside App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Home from "./scenes/home/Home";
 import ItemDetails from "./scenes/itemDetails/ItemDetails";
 import Checkout from "./scenes/checkout/Checkout";
 import Confirmation from "./scenes/checkout/Confirmation";
-import Navbar from "./components/Navbar";
+import Navbar from "./scenes/global/Navbar";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
