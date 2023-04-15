4bd33f65098f5ed652d37bde9eb894322b8b7694
Cheng Lim <93038309+therealcheng@users.noreply.github.com>
2023-04-15 20:26:56 +0100

    Redux toolkit in src/index

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -19,7 +19,7 @@ export const cartSlice = createSlice({
     }, // function to add item to cart
 
     removeFromCart: (state, action) => {
-      state.cart = state.cart.filter((item) => item.id !== action.payload.id);
+      state.cart = state.cart.filter((item) => item.id !== action.payload.id); // function to remove item from cart
     },
 
     increaseCount: (state, action) => {
@@ -28,7 +28,7 @@ export const cartSlice = createSlice({
           item.count++;
         }
         return item;
-      });
+      }); // function to increase count
     },
 
     decreaseCount: (state, action) => {
@@ -37,12 +37,12 @@ export const cartSlice = createSlice({
           item.count--;
         }
         return item;
-      });
+      }); // function to decrease count
     },
 
     setIsCartOpen: (state) => {
       state.isCartOpen = !state.isCartOpen;
-    },
+    }, // function to toggle cart open
   },
 });
 
