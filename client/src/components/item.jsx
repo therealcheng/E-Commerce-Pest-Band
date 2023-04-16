18fde7e4348919e813d1e3b9d145da4148e78d37
Cheng Lim <93038309+therealcheng@users.noreply.github.com>
2023-04-16 11:43:00 +0100

    Create item.jsx
    Reusable component for each item card for commerce website

---
+++ client/src/components/item.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { IconButton, Box, Typography, useTheme, Button } from "@mui/material";
+import AddIcon from "@mui/material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { shades } from "../theme";
+import { addToCart } from "../state";
+import { useNavigate } from "react-router-dom";
+
+const item = ({ item, width }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [count, setCount] = useState(1);
+  const [isHovered, setIsHovered] = useState(false);
+  const {
+    palette: { neutral },
+  } = useTheme();
+
+  const { category, price, name, image } = item.attributes;
+  const {
+    data: {
+      attributes: {
+        formats: {
+          medium: { url },
+        },
+      },
+    },
+  } = image;
+
+  return (
+    <Box width={width}>
+      <Box
+        position="relative"
+        onMouseOver={() => setIsHovered(true)}
+        onMouseOut={() => setIsHovered(false)}
+      >
+        <img
+          alt={item.name}
+          width="600px"
+          height="600px"
+          src={`http://localhost:1337${url}`}
+          onClick={() => navigate(`/item/${item.id}`)}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default item;
