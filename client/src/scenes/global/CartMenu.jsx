3e4862e06f7c6fbb2adf324cf2025109caba7009
Cheng Lim <93038309+therealcheng@users.noreply.github.com>
2023-04-16 10:01:59 +0100

    Create CartMenu.jsx

---
+++ client/src/scenes/global/CartMenu.jsx
@@ -0,0 +1,36 @@
+import { Box, Button, Divider, IconButton, Typography } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import CloseIcon from "@mui/icons-material/Close";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import styled from "@emotion/styled";
+import { shaders } from "../../theme";
+import {
+  decreaseCount,
+  increaseCount,
+  removeFromCart,
+  setIsCartOpen,
+} from "../../state";
+import { useNavigate } from "react-router-dom";
+
+// styled components
+const FlexBox = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const CartMenu = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart);
+  const isCartOpen = useSelector((state) => state.cart.isCartOpen);
+
+  const totalPrice = cart.reduce((total, item) => {
+    return total + item.count * item.attributes.price;
+  }, 0);
+
+  return <Box display={isCartOpen ? "block" : "none"}></Box>;
+};
+
+export default CartMenu;
