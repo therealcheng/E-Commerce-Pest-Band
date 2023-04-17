a871f7aa211946f2052afda59d74f8fd92afb7bb
Cheng Lim <93038309+therealcheng@users.noreply.github.com>
2023-04-17 20:17:55 +0100

    Finished writing item.jsx component

--- client/src/components/item.jsx
+++ client/src/components/item.jsx
@@ -40,7 +40,53 @@ const item = ({ item, width }) => {
           height="600px"
           src={`http://localhost:1337${url}`}
           onClick={() => navigate(`/item/${item.id}`)}
+          style={{ cursor: "pointer" }}
         />
+        <Box
+          display={isHovered ? "blocked" : "none"}
+          position="absolute"
+          bottom="10%"
+          left="0"
+          width="100%"
+          padding="0 5%"
+        >
+          <Box display="flex" justifyContent="space-between">
+            {/* Amount */}
+            <Box
+              display="flex"
+              alignItems="center"
+              backgroundColor={shades.neutral[100]}
+              borderRadius="3px"
+            >
+              <IconButton onClick={() => setCount(Math.max(count - 1, 1))}>
+                <RemoveIcon />
+              </IconButton>
+              <Typography color={shades.primary[300]}>{count}</Typography>
+              <IconButton onClick={() => setCount(Math.max(count + 1))}>
+                <AddIcon />
+              </IconButton>
+            </Box>
+            {/* Button */}
+            <Button
+              onClick={() => {
+                dispatch(addToCart({ item: { ...item, count } }));
+              }}
+              sx={{ backgroundColor: shades.primary[300], color: "white" }}
+            >
+              Add to Cart
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+
+      <Box mt="3px">
+        <Typography variant="subtitle2" color={neutral.dark}>
+          {category
+            .replace(/([A-Z])/g, " $1")
+            .replace(/^./, (str) => str.toUpperCase())}
+        </Typography>
+        <Typography>{name}</Typography>
+        <Typography fontWeight="bold">${price}</Typography>
       </Box>
     </Box>
   );
