af31d7b15f8ea9c713970e7ab8a644f3586699de
Cheng Lim <93038309+therealcheng@users.noreply.github.com>
2023-04-19 09:41:26 +0100

    Carousel home component for future use.

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import MainCarousel from "./MainCarousel";
 
 const Home = () => {
-  return <div></div>;
+  return <div className="home"></div>;
 };
 
 export default Home;
